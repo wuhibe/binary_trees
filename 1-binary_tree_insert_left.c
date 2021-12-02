5ef4a5c78937918bf4ae5c9658c9c3a166ca90d2
wuhibe <wuhibetamire710@gmail.com>
2021-12-02 23:26:43 +0300

    last for today

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,35 +1,27 @@
 #include "binary_trees.h"
 /**
- * binary_tree_node - insert a left node
- * @parent: parent node
+ * binary_tree_insert_left - function to add a binary tree node on the left
+ * @parent: parent node to add new node to
  * @value: value of new node
- * Return: pointer to new node
+ * Return: pointer to newly added node
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (parent == NULL)
-		return (NULL);
-	new = malloc(sizeof(binary_tree_t));
+	new = binary_tree_node(parent, value);
 	if (new == NULL)
 		return (NULL);
 	if (parent->left == NULL)
 	{
 		parent->left = new;
-		new->parent = parent;
-		new->n = value;
-		new->right = NULL;
-		new->left = NULL;
 	}
 	else
 	{
 		new->left = parent->left;
 		parent->left->parent = new;
 		parent->left = new;
-		new->n = value;
-		new->parent = parent;
-		new->right = NULL;
 	}
+
 	return (new);
 }
--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,7 +23,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (tree == NULL || binary_tree_is_leaf(tree))
+	if (tree == NULL)
 		return (0);
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
@@ -33,27 +33,20 @@ size_t binary_tree_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
- * @tree: pointer to the root node of the tree to check
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node of the tree to measure the balance factor of
  *
- * Return: 1 if perfect, 0 otherwise. If tree is NULL, return 0
+ * Description: If tree is NULL, return 0
+ *
+ * Return: balance factor of a binary tree
  */
-int binary_tree_is_perfect(const binary_tree_t *tree)
+int binary_tree_balance(const binary_tree_t *tree)
 {
-	binary_tree_t *l, *r;
+	int left, right;
 
 	if (tree == NULL)
 		return (0);
-	l = tree->left;
-	r = tree->right;
-	if (binary_tree_is_leaf(tree))
-		return (1);
-	if (l == NULL || r == NULL)
-		return (0);
-	if (binary_tree_height(l) == binary_tree_height(r))
-	{
-		if (binary_tree_is_perfect(l) && binary_tree_is_perfect(r))
-			return (1);
-	}
-	return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	return (left - right);
 }
